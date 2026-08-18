d9e87fb8fa72c747b3df3fb0089fb07ca1442a1c
zander120405-coder <zander120405@gmail.com>
2026-08-18 20:13:43 +0200

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -18,16 +18,51 @@ export default function Home() {
       <NavBar />
 
       <main>
+        {/* Hero */}
         <Hero />
+
+        {/* Trust bar */}
         <StatsBar />
-        <Services />
-        <FeaturedProjects />
-        <About />
-        <InsuranceExpertise />
-        <ClientExperience />
-        <WhyChooseUs />
-        <CallToAction />
-        <Contact />
+
+        {/* Services - slightly tighter */}
+        <div className="[&>section]:!py-16 md:[&>section]:!py-20">
+          <Services />
+        </div>
+
+        {/* Projects */}
+        <div className="[&>section]:!py-16 md:[&>section]:!py-20">
+          <FeaturedProjects />
+        </div>
+
+        {/* About - remove excessive white space */}
+        <div className="[&>section]:!pt-16 [&>section]:!pb-14 md:[&>section]:!pt-20 md:[&>section]:!pb-16">
+          <About />
+        </div>
+
+        {/* Insurance expertise - tighter dark section */}
+        <div className="[&>section]:!py-16 md:[&>section]:!py-20">
+          <InsuranceExpertise />
+        </div>
+
+        {/* Client experience - stronger but compact */}
+        <div className="[&>section]:!py-16 md:[&>section]:!py-20 [&_h3]:text-lg [&_p]:leading-relaxed">
+          <ClientExperience />
+        </div>
+
+        {/* Why Herla - reduce empty space before CTA */}
+        <div className="[&>section]:!pt-16 [&>section]:!pb-14 md:[&>section]:!pt-20 md:[&>section]:!pb-16">
+          <WhyChooseUs />
+        </div>
+
+        {/* CTA - compact */}
+        <div className="[&>section]:!py-14 md:[&>section]:!py-16">
+          <CallToAction />
+        </div>
+
+        {/* Contact - compact final section */}
+        <div className="[&>section]:!py-16 md:[&>section]:!py-20">
+          <Contact />
+        </div>
       </main>
 
       <Footer />
