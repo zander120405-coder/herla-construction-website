40234d2bf946fca8a0f63569525fa241b0ce93c4
zander120405-coder <zander120405@gmail.com>
2026-08-18 13:51:03 +0200

    Refactor Home component by removing unused imports
    Removed unused components and reorganized imports.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,38 +1,26 @@
 import { NavBar } from "@/components/NavBar";
 import { Hero } from "@/components/Hero";
-import { WhyChooseUsStrip } from "@/components/WhyChooseUsStrip";
+
 import { StatsBar } from "@/components/StatsBar";
 import { Services } from "@/components/Services";
 import { About } from "@/components/About";
-import { VisionMission } from "@/components/VisionMission";
-import { Projects } from "@/components/Projects";
 import { FeaturedProjects } from "@/components/FeaturedProjects";
-import { WorkStrip } from "@/components/WorkStrip";
 import { WhyChooseUs } from "@/components/WhyChooseUs";
-import { Testimonials } from "@/components/Testimonials";
-import { FAQ } from "@/components/FAQ";
 import { CallToAction } from "@/components/CallToAction";
 import { Contact } from "@/components/Contact";
 import { Footer } from "@/components/Footer";
 import { WhatsAppFloat } from "@/components/WhatsAppFloat";
-
 export default function Home() {
   return (
     <div className="bg-background min-h-screen text-foreground selection:bg-primary selection:text-primary-foreground">
       <NavBar />
       <main>
         <Hero />
-        <WhyChooseUsStrip />
         <StatsBar />
         <Services />
-        <About />
-        <VisionMission />
-        <Projects />
         <FeaturedProjects />
-        <WorkStrip />
+        <About />
         <WhyChooseUs />
-        <Testimonials />
-        <FAQ />
         <CallToAction />
         <Contact />
       </main>
