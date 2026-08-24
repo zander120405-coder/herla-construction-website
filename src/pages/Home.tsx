93f55652d30b086c826a2cd447902de927054330
zander120405-coder <zander120405@gmail.com>
2026-08-24 20:28:32 +0200

    Update Home component layout and styling

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,9 +4,29 @@ import { StatsBar } from "@/components/StatsBar";
 
 export default function Home() {
   return (
-    <div className="h-screen overflow-hidden bg-background text-foreground">
+    <div className="h-screen overflow-hidden bg-[#06111f] text-white">
+      <div className="flex h-full flex-col">
+        <NavBar />import { NavBar } from "@/components/NavBar";
+import { Hero } from "@/components/Hero";
+import { StatsBar } from "@/components/StatsBar";
+
+export default function Home() {
+  return (
+    <div className="h-screen overflow-hidden bg-[#06111f] text-white">
       <div className="flex h-full flex-col">
         <NavBar />
+
+        <main className="flex min-h-0 flex-1 flex-col">
+          <div className="min-h-0 flex-1">
+            <Hero />
+          </div>
+
+          <StatsBar />
+        </main>
+      </div>
+    </div>
+  );
+}
 
         <main className="flex min-h-0 flex-1 flex-col">
           <div className="min-h-0 flex-1">
